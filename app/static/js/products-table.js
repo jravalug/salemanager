c235d47751a17cb25c0d3442bd8024772fc9436d
Jose Abadia <jr.avalug@gmail.com>
2025-03-02 15:59:05 -0500

    testing simple-datatable

--- app/static/js/products-table.js
+++ app/static/js/products-table.js
@@ -1,38 +1,31 @@
-if (
-      document.getElementById("product_filter-table") &&
-      typeof simpleDatatables.DataTable !== "undefined"
-    ) {
-      const dataTable = new simpleDatatables.DataTable("#product_filter-table", {
-        paging: true,
-        perPage: 5,
-        perPageSelect: [5, 10, 15, 20, 25],
-        sortable: true,
-        template: (options, dom) => `<div class='${options.classes.top}'>
-            ${
-            options.paging && options.perPageSelect ?
-                `<div class='${options.classes.dropdown}'>
-                    <label>
-                        <select class='${options.classes.selector}'></select> ${options.labels.perPage}
-                    </label>
-                </div>` :
-                ""
-        }
-            ${
-            options.searchable ?
-                `<div class='${options.classes.search}'>
-                    <input class='${options.classes.input}' placeholder='${options.labels.placeholder}' type='search' title='${options.labels.searchTitle}'${dom.id ? ` aria-controls="${dom.id}"` : ""}>
-                </div>` :
-                ""
-        }
-        </div>
-        <div class='${options.classes.container}'${options.scrollY.length ? ` style='height: ${options.scrollY}; overflow-Y: auto;'` : ""}></div>
-        <div class='${options.classes.bottom}'>
-            ${
-            options.paging ?
-                `<div class='${options.classes.info}'></div>` :
-                ""
-        }
-            <nav class='${options.classes.pagination}'></nav>
-        </div>`,
+import { DataTable } from "../../../node_modules/simple-datatables/dist/module.js";
+
+const businessId = document.getElementById("business-id").value
+
+fetch(`/api/business/${businessId}/product/products`)
+  .then((response) => response.json())
+  .then((data) => {
+    window.dt = new DataTable("#demo-table", {
+      data: {
+        headings: [
+          {
+            text: "ID",
+            data: "id"
+          },
+          {
+            text: "NOMBRE",
+            data: "name"
+          },
+          {
+            text: "PRECIO",
+            data: "price"
+          },
+          {
+            text: "CATEGORIA",
+            data: "category"
+          }
+        ],
+        data
+      },
     });
-    }
+  });
